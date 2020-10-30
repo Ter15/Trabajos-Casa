a218aa898ffbd76260020e818b1b3dba56439cfd
Ter15 <bonmati20964@gamil.com>
2020-10-30 19:55:11 +0100

    3.4 corregido

--- Ejercicio instituto/Práctica 3.5/Práctica 3.5/Form1.cs
+++ Ejercicio instituto/Práctica 3.5/Práctica 3.5/Form1.cs
@@ -17,27 +17,35 @@ namespace Práctica_3._5
             InitializeComponent();
         }
 
+        //hacer una función que se le pasen 3 enteros y que devuelva el mayor d elos 3
+               
+            public static int EsMayorDe3(int a, int b, int c)
+            {
+                if (a > b)
+                {
+                    if (a > c)
+                        return a;
+                    else
+                        return c;
+                }
+                else
+                {
+                    if (b > c)
+                        return b;
+                    else
+                        return c;
+                }
+            }
+
         private void button1_Click(object sender, EventArgs e)
         {
             int numero1 = int.Parse(textBox1.Text);
             int numero2 = int.Parse(textBox2.Text);
             int numero3 = int.Parse(textBox3.Text);
-            if (numero1 > numero2)
-            {
-                if (numero3 > numero1)
-                {
-                    MessageBox.Show(" el numero mayor es el: " + numero3);
-                }
-                else if (numero2 > numero3)
-                {
-                    MessageBox.Show(" el numero mayor es el: " + numero1);
-                }
-                else
-                {
-                    MessageBox.Show(" el numero mayor es el: no se que poner");
-                }
 
-            }
+            int mayor = EsMayorDe3(numero1, numero2, numero3);
+
+            MessageBox.Show(" el numero mayor es el: " + mayor);
         }
     }
 }
