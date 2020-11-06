95e7ab80862d8eb1dbac242e8152d366c8637804
Ter15 <bonmati20964@gmail.com>
2020-11-06 20:34:13 +0100

    EJERCICIO 3.8 CASI ACABADO

---
+++ Ejercicio instituto/Práctica 3.10/Práctica 3.10/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Realizar un programa en el que el usuario introduzca una cantidad en euros (con decimales) y
+//obtengamos los billetes y monedas de euro correspondientes, así como las monedas de céntimos
+//correspondientes.
+namespace Práctica_3._10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Ejercicio instituto/Práctica 3.9/Práctica 3.9/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Realizar un programa que pida una cantidad de dinero (en pesetas), y muestre en una etiqueta
+//cuantos billetes de 10.000, de 5.000, de 2.000, de 1000 y monedas de 100 y 25 le corresponden.
+//Por ejemplo: 27.375 le corresponde 2 de 10.000, 1 de 5.000, 1 de 2.000, 3 de 100 y 3 de 25.
+//Hacerlo de forma que si corresponde 0 billetes no aparezca
+namespace Práctica_3._9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
